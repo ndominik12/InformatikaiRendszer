0152656bedfa13f8da74032237b69789fb097472
ndominik12 <ndomi560@gmail.com>
2026-03-11 11:14:44 +0100

    feladat 1 is ready

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace InformatikaiRendszer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("kolcsonzes.txt");
+            int[] adatok = new int[sorok.Length];
+            for (int i = 0; i < sorok.Length; i++)
+            {
+                adatok[i] = int.Parse(sorok[i].Trim());
+            }
+
+            Console.WriteLine("Beolvasas kesz.");
+        }
+    }
+}
