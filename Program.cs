71a805a62c65efb7fe00ed052e124a2552528747
ndominik12 <ndomi560@gmail.com>
2026-03-11 11:18:37 +0100

    feladat 4 is ready

--- Program.cs
+++ Program.cs
@@ -31,6 +31,27 @@ namespace InformatikaiRendszer
             Console.WriteLine();
             Console.WriteLine("3. feladat:");
             Console.WriteLine($"Meresi hibak szama: {hibak}");
+
+            string[] idok = {
+                "9:00-9:10", "9:10-9:20", "9:20-9:30", "9:30-9:40",
+                "9:40-9:50", "9:50-10:00", "10:00-10:10", "10:10-10:20",
+                "10:20-10:30", "10:30-10:40", "10:40-10:50", "10:50-11:00"
+            };
+
+            int maxErtek = -1;
+            int maxIndex = 0;
+            for (int i = 0; i < adatok.Length; i++)
+            {
+                if (adatok[i] != -1 && adatok[i] > maxErtek)
+                {
+                    maxErtek = adatok[i];
+                    maxIndex = i;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("4. feladat:");
+            Console.WriteLine($"Legtobb kolcsonzes: {idok[maxIndex]} ({maxErtek} konyv)");
         }
     }
 }
